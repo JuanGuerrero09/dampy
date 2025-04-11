db2ac169851bcd161e72d718de56d03cf50e935c
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2025-04-11 12:15:59 +0200

    init project

---
+++ main.py
@@ -0,0 +1,17 @@
+# Generic Packages
+import numpy as np
+import pandas as pd
+
+# geospacial packages
+import rasterio
+from matplotlib import pyplot as plt
+from rasterio.plot import show
+
+
+def main():
+
+    print("Hello from dampy!")
+
+
+if __name__ == "__main__":
+    main()
