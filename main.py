d28ab38ba5bc484558974b2cd75dd98321e04c2d
juanguerrero09 <juanguerrero09mc@gmail.com>
2025-04-12 23:03:11 +0200

    added initial flood modeling

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import pandas as pd
 
 # geospacial packages
 import rasterio
-from matplotlib import pyplot as plt
-from rasterio.plot import show
 
 # import landlab packages
 from landlab import RasterModelGrid, imshow_grid
 from landlab.components import (
-    
-    FlowAccumulator,
     DepressionFinderAndRouter,
     FastscapeEroder,
+    FlowAccumulator,
     LinearDiffuser,
     StreamPowerEroder,
 )
+# import matplotlib
+# matplotlib.use('TkAgg')  # Cambiar a un backend interactivo
+from matplotlib import pyplot as plt
+from rasterio.plot import show
+
 
 def describe_raster(filename):
     """Describe the raster file and print its metadata.
@@ -26,7 +28,7 @@ def describe_raster(filename):
     """
     with rasterio.open(filename) as src:
         print("Profile: \n", src.profile)
-        print("Size: \n",src.width, src.height)
+        print("Size: \n", src.width, src.height)
         print("Bounds: \n", src.bounds)
         print("Coordinate Reference System: \n", src.crs)
         print("Transform: \n", src.transform)
@@ -41,25 +43,24 @@ def describe_raster(filename):
         print("Driver: \n", src.driver)
         print("Tags: \n", src.tags())
         print("Tags (all): \n", src.tags(ns=None))
-        print("Tags (ns): \n", src.tags(ns=''))
-        print(src.read(1))
+        print("Tags (ns): \n", src.tags(ns=""))
 
-def openRaster(filename):
-    """Open a raster file and return the data and metadata.
+def describe_elevation(elevArr):
+    """Describe the elevation array and print its metadata.
 
     Args:
-        filename (str): The path to the raster file.
-
-    Returns:
-        tuple: A tuple containing the data and metadata of the raster file.
+        elevArr (numpy.ndarray): The elevation array.
     """
-    with rasterio.open(filename) as src:
-        show(src)
-        data = src.read(1)
-        meta = src.meta
-    return data, meta
+    print("Elevation Array: \n", elevArr)
+    print("Shape: \n", elevArr.shape)
+    print("Data Type: \n", elevArr.dtype)
+    print("No. of NoData Values: \n", np.isnan(elevArr).sum())
+    print("Min Value: \n", np.nanmin(elevArr))
+    print("Max Value: \n", np.nanmax(elevArr))
+    print("Mean Value: \n", np.nanmean(elevArr))
+    print("Std Value: \n", np.nanstd(elevArr))
 
-def createGridFromRaster(rasterObj, metadata):
+def createGridFromRaster(ncols: float, nrows: float, dxy: float, elevArr:np.ndarray = None):
     """Create a RasterModelGrid from the elevation array and metadata.
 
     Args:
@@ -69,20 +70,42 @@ def createGridFromRaster(rasterObj, metadata):
     Returns:
         RasterModelGrid: A RasterModelGrid object created from the elevation array and metadata.
     """
-    grid = RasterModelGrid((metadata['height'], metadata['width']), xy_spacing=(metadata['transform'][0], metadata['transform'][4]))
-    grid.add_field('topographic__elevation', rasterObj, at='node')
-    return grid
+
+    # define a landlab raster
+    # create a landlab grid
+    mg = RasterModelGrid(
+        shape=(nrows, ncols),
+        xy_spacing=dxy,
+        # xy_of_lower_left=(rasterObj.bounds[0], rasterObj.bounds[1]),
+        xy_of_lower_left=(0, 0),
+    )
+    print(nrows, ncols, dxy)
+    # create dataset with zero values
+    zr = mg.add_zeros("topographic__elevation", at="node")
+    # add the elevation data to the grid
+    if elevArr is not None:
+        # convert the elevation array to a 1D array
+        zr += elevArr[::-1,:].ravel()
+
+        imshow_grid(mg, "topographic__elevation", shrink=0.5, at="node")
+    return mg
 
 
 def main():
     # Open the raster file and get the data and metadata
-    filename = './assets/rst/clippedDem_12N.tif'
-    rasterObj = openRaster(filename)
-    describe_raster(filename)
-    elevArr = rasterObj[0] # <class 'numpy.ndarray'>
-    meta = rasterObj[1] # <class 'dict'>
-    show(elevArr, cmap='terrain')
-    
+    filename = "./assets/rst/clippedDem_12N.tif"
+    rasterObj = rasterio.open(filename)
+    show(rasterObj)
+
+    # describe_raster(filename)
+    elevArr = rasterObj.read(1)  # <class 'numpy.ndarray'>
+    describe_elevation(elevArr)
+    meta = rasterObj.meta
+    print(meta)
+    show(elevArr, cmap="terrain")
+
+    dxy = rasterObj.res  # side length of a raster model cell, or resolution [m], initially 50
+    createGridFromRaster(rasterObj.width, rasterObj.height, dxy)
 
 
 if __name__ == "__main__":
